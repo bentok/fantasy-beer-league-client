29329a62cc3b0404c17139fc54292275f56455dd
Ben Copeland <bscopeland@gmail.com>
2020-03-15 18:35:03 -0400

    Fix Fulma

--- src/main.fs
+++ src/main.fs
@@ -1,7 +1,10 @@
 module Main
 
 open Fable.React
+open Fable.Core.JsInterop
 open App
+
+importAll "./main.scss"
 
 let document = Browser.Dom.window.document
 let root = document.getElementById "root"
