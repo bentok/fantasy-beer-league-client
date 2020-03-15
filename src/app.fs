0f4f8d81e253f6a92c835994b2e4b27fe5c8ff62
Ben Copeland <bscopeland@gmail.com>
2020-03-15 19:54:30 -0400

    Add first fulma element

--- src/app.fs
+++ src/app.fs
@@ -8,22 +8,34 @@ open ReactRouter.Switch
 open ReactRouter.Route
 open Fulma
 
+let home =
+    Hero.hero [ Hero.Color IsSuccess
+                Hero.IsMedium ]
+        [ Hero.head [ ]
+            [ Tabs.tabs [ Tabs.IsBoxed
+                          Tabs.IsCentered ]
+                [ Tabs.tab [ Tabs.Tab.IsActive true ]
+                    [ Link [To (!^ "/")] [str "League" ] ]
+                  Tabs.tab [ ]
+                    [ Link [To (!^ "/team")] [str "Team"] ]
+                  Tabs.tab [ ]
+                    [ a [ ]
+                        [ str "Match-ups" ] ]
+                  Tabs.tab [ ]
+                    [ a [ ]
+                        [ str "Settings" ] ] ] ]
+          Hero.body [ ]
+            [ Container.container [ Container.IsFluid
+                                    Container.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
+                [ Heading.h1 [ ]
+                    [ str "Fantasy Beer League" ]
+                  Heading.h2 [ Heading.IsSubtitle ]
+                    [ str "Subtitle Here" ] ] ] ]
+
 let App() =
     BrowserRouter [] [
-        div [] [str "Hello from Fable-React Application!"]
-        Link [To (!^ "/about")] [str "About"]
-        Link [To (!^ "/")] [str "Home" ]
         Switch [] [
-            Route [Path "/about"] [str "Content of the About page!"]
-            Route [Path "/"] [str "Content of the Home page!"]
+            Route [Path "/team"] [str "Content of the Team page!"]
+            Route [Path "/"] [(home)]
         ]
-        
-        // TODO: Make sure webpack loads css into index.html
-        Tag.list
-            [ Tag.List.HasAddons ]
-            [ Tag.tag 
-                [ Tag.Color IsDanger ]
-                [ str "High priority" ]
-              Tag.delete [ ] [ ]
-            ]
     ]
