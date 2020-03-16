94fd0f8fda86ef5bcb99d722bb670f03d76e11f6
Ben Copeland <bscopeland@gmail.com>
2020-03-15 20:20:05 -0400

    Move home page to it's own file

--- src/app.fs
+++ src/app.fs
@@ -8,29 +8,7 @@ open ReactRouter.Switch
 open ReactRouter.Route
 open Fulma
 
-let home =
-    Hero.hero [ Hero.Color IsSuccess
-                Hero.IsMedium ]
-        [ Hero.head [ ]
-            [ Tabs.tabs [ Tabs.IsBoxed
-                          Tabs.IsCentered ]
-                [ Tabs.tab [ Tabs.Tab.IsActive true ]
-                    [ Link [To (!^ "/")] [str "League" ] ]
-                  Tabs.tab [ ]
-                    [ Link [To (!^ "/team")] [str "Team"] ]
-                  Tabs.tab [ ]
-                    [ a [ ]
-                        [ str "Match-ups" ] ]
-                  Tabs.tab [ ]
-                    [ a [ ]
-                        [ str "Settings" ] ] ] ]
-          Hero.body [ ]
-            [ Container.container [ Container.IsFluid
-                                    Container.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
-                [ Heading.h1 [ ]
-                    [ str "Fantasy Beer League" ]
-                  Heading.h2 [ Heading.IsSubtitle ]
-                    [ str "Subtitle Here" ] ] ] ]
+open Home
 
 let App() =
     BrowserRouter [] [
